018233e7212598629ea3e8351dfdd270a4266939
Breno Marques <132229523+brenomarq@users.noreply.github.com>
2023-06-30 14:30:18 -0300

    Create dict_q3.py

---
+++ dict_q3.py
@@ -0,0 +1,23 @@
+# Lista de Dicionários - Questão 3
+ordem = input().split()
+pedido = input().split()
+produtos = {}
+pedidos = {}
+
+i = 1
+while i < len(ordem):
+    produtos[ordem[i-1]] = float(ordem[i])
+    i += 2
+
+i = 1
+while i < len(pedido):
+    pedidos[pedido[i-1]] = int(pedido[i])
+    i += 2
+
+
+valor = 0
+for produto in produtos:
+    if produto in pedidos:
+        valor += (produtos[produto] * pedidos[produto])
+
+print(f"R$ {valor:.2f}")
