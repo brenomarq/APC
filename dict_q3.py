af8b5974f3d62573e3b12fdaa913950f9305ce7b
Breno Marques <132229523+brenomarq@users.noreply.github.com>
2023-07-07 13:11:37 -0300

    Update dict_q3.py

--- dict_q3.py
+++ dict_q3.py
@@ -7,13 +7,9 @@ pedidos = {}
 i = 1
 while i < len(ordem):
     produtos[ordem[i-1]] = float(ordem[i])
+    if i < len(pedido):
+        pedidos[pedido[i-1]] = int(pedido[i])    
     i += 2
-
-i = 1
-while i < len(pedido):
-    pedidos[pedido[i-1]] = int(pedido[i])
-    i += 2
-
 
 valor = 0
 for produto in produtos:
